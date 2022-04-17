43803ef42d747ad404b67d1328cb98ea3b509877
leemanshow <354621693@qq.com>
2022-04-18 00:32:34 +0800

    初次提交

---
+++ dbcommon/src/main/java/com/leemanshow/dbcommon/DbcommonApplication.java
@@ -0,0 +1,13 @@
+package com.leemanshow.dbcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbcommonApplication.class, args);
+    }
+
+}
